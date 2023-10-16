b1b249b9a3c1570bdd5316e9e3540dbbd12b168d
Etienne Chassaing <60154720+cetiennec@users.noreply.github.com>
2023-10-16 18:00:31 +0200

    installed on windows

--- robotinterface/protocol.py
+++ robotinterface/protocol.py
@@ -3,7 +3,7 @@
 
 import os
 import sys 
-#sys.path.append(os.path.join(sys.path[0],'..'))
+sys.path.append(os.path.join(sys.path[0],'..'))
 #sys.path.append(r"/Users/Etienne/Documents/GitHub/robot-interface")
 import logging
 import asyncio
