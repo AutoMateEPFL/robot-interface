9f8c385e9312e6d117d05759bba12b6a742daa5d
Puhuhuhu <80117329+Puhuhuhu@users.noreply.github.com>
2024-03-15 14:46:03 +0100

    added take_picture

---
+++ robotinterface/take_picture.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import platform
+from experiment_helper import *
+if platform.system() == 'Windows':
+    sys.path.append(os.path.join(sys.path[0], '..'))
+
+import logging
+import asyncio
+from robotinterface.hardware_control.robot import Robot
+from robotinterface.logistics.grid import Grid, GridPosition
+from robotinterface.logistics.grid_utils import fill_grid, get_plateholder_petri_pos
+from robotinterface.gui.gui import InteractiveWindow
+from concurrent.futures import ThreadPoolExecutor
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+async def main():
+    # Def grid :
+    grid = Grid(x_max=-800, x_dist=-199, y_max=-620, y_dist=-200)
+
+    # Camera position will be used as the pick and place position:
+    pic_pos = GridPosition(2, 1)
+    grid.set_camera_position(pic_pos)
+
+    # Set the stacks positions
+    grid.set_stack_positions([GridPosition(3, i) for i in range(0,4)]+[GridPosition(4, i) for i in range(0,4)])
+
+    # async loop:
+    loop = asyncio.get_running_loop()
+    executor = ThreadPoolExecutor(max_workers=2)
+
+    # Robot build
+    if platform.system() == 'Windows':
+        robot = await Robot.build(grid)
+
+
+
+    await robot.take_picture(obj=target[0], obj_rem=target[1], folder_name=target[0]._associated_experiment,
+                            prefix="marker_" + str(target[0].number) + "_",
+                            suffix="_" + str(target[0].associated_name), to_save = True, pic_pos=pic_pos)
+    # print('place ', target[0]._associated_experiment, target[0].associated_name,  'to ', stack_pos.x_id, stack_pos.y_id)
+    
+    # shutdown the robot
+    await robot.shutdown()
+
+
+
+
+
+asyncio.run(main())
+    
--- robotinterface/take_pictures_of_one_experiment.bat
+++ robotinterface/take_pictures_of_one_experiment.bat
@@ -1,5 +1,5 @@
 @echo off
 call C:\Users\"AutoMate EPFL"\Documents\GitHub\robot-interface\robot-env\Scripts\activate
 cd "C:\Users\AutoMate EPFL\Documents\GitHub\robot-interface"
-python robotinterface\protocol_one_experiment.py
+python robotinterface\take_picture.py
 pause
